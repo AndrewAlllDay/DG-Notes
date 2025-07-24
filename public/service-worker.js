03d3a19ab7036986f3fde12533d84cb7dbf17ad2
AndrewAlllDay <andrewsnyderdesign@gmail.com>
2025-07-24 07:45:11 -0400

    app version update

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,5 @@
 // IMPORTANT: Increment this CACHE_NAME any time you make changes to your app's code or assets
-const CACHE_NAME = 'dgnotes-cache-v1.0.22'; // <-- We will increment this again before the final deploy
+const CACHE_NAME = 'dgnotes-cache-v1.0.23'; // <-- We will increment this again before the final deploy
 const urlsToCache = [
     '/',
     '/index.html',
